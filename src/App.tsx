c614d1de472f25a31a1385f123b78d528594299a
sanderdechering <sander.dechering@gmail.com>
2023-03-13 09:24:19 +0100

    -Added a filterButtons state, which tracks the active state of the button. When a button is active, set the boolean to true. -Added items.length in the footer, which tracks the amount of todo. -Fixed the bug where the gradient was not working on the check buttons. -Clear completed function sets the active state of button 'all' to true.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,31 @@ import iconMoon from './assets/icon-moon.svg'
 import iconSun from './assets/icon-sun.svg'
 import iconChecked from './assets/icon-check.svg'
 import backgroundImage_desktop_dark from './assets/bg-desktop-dark.jpg'
-import {useState} from "react";
+import React, {useState} from "react";
+
 function App() {
     const [items, setItems] = useState([
         {
-            text: 'test item',
+            text: 'My first item',
+            completed: false,
+        },
+        {
+            text: 'My second item',
+            completed: false,
+        },
+        {
+            text: 'Complete the Todo list',
             completed: false,
         }
     ]);
 
     let [displayItems, setDisplayItems] = useState(items)
+
+    const [filterButtons, setFilterButtons] = useState({
+        all: true,
+        active: false,
+        completed: false,
+    });
 
     function addTodo(text: string) {
         const currentItem = [...items]; // get the specified item
@@ -32,23 +47,45 @@ function App() {
         }
     }
 
+
     function displayActiveItems(){
         const currentItem = items.filter(item => !item.completed);
         setDisplayItems(currentItem); // update the state with the new array
+        setFilterButtons({
+            all: false,
+            active: true,
+            completed: false
+        });
     }
     function displayCompletedItems(){
         const currentItem = items.filter(item => item.completed);
         setDisplayItems(currentItem); // update the state with the new array
+        setFilterButtons({
+            all: false,
+            active: false,
+            completed: true
+        });
     }
     function displayAllItems(){
         setDisplayItems(items) // update state with items array, so all items are displayed
+        setFilterButtons({
+            all: true,
+            active: false,
+            completed: false
+        });
     }
 
     function clearCompleted() {
         const filteredItems = items.filter(item => !item.completed);
         setItems(filteredItems);
         setDisplayItems(filteredItems)
-        console.log('hello')
+        if (filterButtons.completed) {
+            setFilterButtons({
+                all: true,
+                active: false,
+                completed: false
+            })
+        }
     }
 
     function toggleCompleted(index:number) {
@@ -56,6 +93,7 @@ function App() {
         const currentItem = updatedItems[index]; // get the specified item
         currentItem.completed = !currentItem.completed; // toggle the completed property of the item
         setItems(updatedItems); // update the state with the new array
+
     }
 
     return (
@@ -80,7 +118,8 @@ function App() {
                     <div className="flex flex-col bg-DarkMyVeryDarkDesaturatedBlue mt-8 rounded-t-md">
                         {displayItems.map((item, index) =>(
                             <div key={index} className={`py-4 border-b-[1px] border-DarkMyVeryDarkGrayishBlue flex flex-row ${item.completed ? 'line-through text-DarkMyVeryDarkGrayishBlue2' : ''}` }>
-                                <div className={`flex justify-center ml-6  rounded-full w-6 h-6 absolute ${item.completed ? 'cursor-pointer bg-DarkMyDarkGrayishBlue bg-gradient-to-r from-GradientBlue to-GradientPurple' : 'cursor-pointer bg-DarkMyDarkGrayishBlue hover:bg-gradient-to-r from-GradientBlue to-GradientPurple'}\`` } onClick={() => toggleCompleted(index)}>
+                                <div className={`flex justify-center ml-6  rounded-full w-6 h-6 absolute ${item.completed ? 'cursor-pointer bg-DarkMyDarkGrayishBlue bg-gradient-to-r ' +
+                                    'from-GradientBlue to-GradientPurple' : 'cursor-pointer bg-DarkMyDarkGrayishBlue hover:bg-gradient-to-r from-GradientBlue to-GradientPurple'}` } onClick={() => toggleCompleted(index)}>
                                     <div className={`bg-DarkMyVeryDarkDesaturatedBlue rounded-full w-5 h-5 mt-[2px] absolute ${item.completed ? 'hidden' : ''}` }/>
                                     <img src={iconChecked} className={`rounded-full w-3.5 h-3.5 my-auto  ${item.completed ? '' : 'hidden'}` } alt="icon of a checkmark"/>
                                 </div>
@@ -91,18 +130,16 @@ function App() {
 
                     ): (
                         <div className="flex flex-col bg-DarkMyVeryDarkDesaturatedBlue mt-8 rounded-t-md">
-                             <p className="p-10">Empty list</p>
+                             <p className="p-10 text-xl">No items here...</p>
                         </div>
                     )}
 
-
-
                     <div className="flex flex-row bg-DarkMyVeryDarkDesaturatedBlue justify-around text-DarkMyVeryDarkGrayishBlue py-4 rounded-b-md">
-                        <span>x items left</span>
+                        <span>{items.length} items left</span>
                         <div className="mx-20">
-                            <span className="text-MyBrightBlue cursor-pointer" onClick={displayAllItems}>All</span>
-                            <span className="mx-5 hover:text-DarkMyLightGrayishBlue cursor-pointer" onClick={displayActiveItems}>Active</span>
-                            <span className="hover:text-DarkMyLightGrayishBlue cursor-pointer" onClick={displayCompletedItems}>Completed</span>
+                            <span className={`cursor-pointer ${filterButtons['all'] ? 'text-MyBrightBlue' : 'hover:text-DarkMyLightGrayishBlue'}`} onClick={displayAllItems}>All</span>
+                            <span className={`cursor-pointer mx-5 ${filterButtons['active'] ? 'text-MyBrightBlue' : 'hover:text-DarkMyLightGrayishBlue'}`} onClick={displayActiveItems}>Active</span>
+                            <span className={`cursor-pointer ${filterButtons['completed'] ? 'text-MyBrightBlue' : 'hover:text-DarkMyLightGrayishBlue'}`} onClick={displayCompletedItems}>Completed</span>
                         </div>
                         <span className="hover:text-DarkMyLightGrayishBlue cursor-pointer" onClick={clearCompleted}>Clear Completed</span>
                     </div>
